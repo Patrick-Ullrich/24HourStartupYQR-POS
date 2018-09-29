b80d02afd5191d462dfa9372cfa3cd6902a3fe44
Patrick Ullrich <patrick.ullrich93@gmail.com>
2018-09-29 11:13:23 -0600

    feat:

--- js/router.js
+++ js/router.js
@@ -46,50 +46,7 @@ function bindNavigation () {
 }
 
 function setDrinks () {
-  const drinks = [
-    {
-      'id': 1,
-      'title': 'Burt Reynolds',
-      'description': 'Fizzy but fun.',
-      'price': '4.99',
-      'discount': 10
-    },
-    {
-      'id': 2,
-      'title': 'White Russian',
-      'description': 'Fizzy but fun.',
-      'price': '3.99',
-      'discount': 0
-    },
-    {
-      'id': 3,
-      'title': 'Caesar',
-      'description': 'Fizzy but fun.',
-      'price': '7.00',
-      'discount': 20
-    },
-    {
-      'id': 4,
-      'title': 'Pepsi',
-      'description': 'Fizzy but fun.',
-      'price': '1.99',
-      'discount': 20
-    },
-    {
-      'id': 5,
-      'title': 'Ginger Ale',
-      'description': 'Fizzy but fun.',
-      'price': '1.99',
-      'discount': 0
-    },
-    {
-      'id': 6,
-      'title': 'Fanta',
-      'description': 'Fizzy but fun.',
-      'price': '2.29',
-      'discount': 0
-    }
-  ]
+  
   const dataTitles = document.getElementsByClassName('data-title')
   const dataDescriptions = document.getElementsByClassName('data-description')
   const dataPrice = document.getElementsByClassName('data-price')
